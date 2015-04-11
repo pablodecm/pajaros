6ed719087c3e8a75ecede06b172dd07658d63aa1
Nacho <nachosandres@gmail.com>
2015-04-11 17:43:23 +0200

    revisados programas

--- nnetwork/train_nn.m
+++ nnetwork/train_nn.m
@@ -1,13 +1,72 @@
-num_mfcc=
-col_init_mfcc=
+nfeatures=32
 
-data_cell = read_mixed_csv(fileName,',');
-records=size(data_cell,1);
-inputs=cell2mat(data{:,col_init_mfcc:(col_init_mfcc+num_mfcc)});
+classfileid = fopen('classID.txt');
+classcell = textscan(classfileid,'%s');
+classunique=unique(classcell{1});
+
+classes=size(classunique,1);
+samples=size(classcell{1},1);
 
 
+rnfileid = fopen('listaRN.txt');
+rncell = textscan(rnfileid,'%d');
+
+
+
+featureid = fopen("allfeatures.csv");
+featuredata = textscan(featureid,['LIFECLEF2014_BIRDAMAZON_XC_WAV_RN%d',repmat('%f',[1,nfeatures])],'CollectOutput',1)
+
+
+nsegments=size(featuredata{2},1);
 
 % Solve a Pattern Recognition Problem with a Neural Network
+
+
+
+
+
+
+
+
+
+
+
+
+
+dictionary=zeros(unique,samples);
+
+for j=1:samples
+	for i=1:classes
+		if (strcmp(classunique{i},classcell{1}{j}))
+			dictionary(i,j)=1;
+		end if
+	end for
+end for
+
+
+
+
+rnlist=featuredata{1};
+features=featuredata{2};
+targets=zeros(unique,nsegments);
+for i=1:nsegments
+	for j=1:samples
+		if (featuredata{1}{i}==rncell{1}{j})
+			targets(:,i)=dictionary(:,j);
+		end if
+	end for
+end for
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -16,10 +75,14 @@ hiddenLayerSize = 10;
 net = patternnet(hiddenLayerSize);
 
 
-% Set up Division of Data for Training, Validation, Testing
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 15/100;
-net.divideParam.testRatio = 15/100;
+% Set up Division of Data for Training, Validation, Testing.divide
+traininds=randperm();
+
+
+net.divideFcn='divideind';
+net.divideParam.trainInd = 1:;
+net.divideParam.valInd = :;
+net.divideParam.testInd = :n_seg_total;
 
 
 % Train the Network
